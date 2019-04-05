6f7a76d461f7eeafb536d1c57904fa14ef48af25
borjasolerr <borjasolrey@gmail.com>
2019-04-05 11:35:56 +0200

    Added dbQuesries

---
+++ database/dbQueries.js
@@ -0,0 +1,14 @@
+const db = require('./dbConfig');
+
+module.exports = {
+  addNewUser,
+  getUserByName
+};
+
+function addNewUser(username, password) {
+  return db('users').insert({ username, password });
+}
+
+function getUserByName(username) {
+  return db('users').where({ username });
+}
